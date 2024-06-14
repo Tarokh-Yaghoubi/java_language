6e92bd8f2418336e17458dc2c4e2144ca7e9cbb1
Tarokh Yaghoubi <tarokhgit@gmail.com>
2024-06-14 12:11:46 +0330

    Introduction to Classes and Object-Oriented Programming in Java

---
+++ Classes/introduction_to_classes.java
@@ -0,0 +1,9 @@
+package java_language.Classes;
+
+public class introduction_to_classes {
+    // a class defines a new data type
+    // thus a class is a template for an object, and an object is an instance of a class
+    // object == instance
+
+
+}
