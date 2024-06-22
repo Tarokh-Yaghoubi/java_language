08cd658299907439ecf091e31f2f530eb8f98c73
Tarokh Yaghoubi <tarokhgit@gmail.com>
2024-06-22 14:21:17 +0330

    Returning objects of classes | MEthods returning class objects

--- Classes/ReturnObjects.java
+++ Classes/ReturnObjects.java
@@ -1,4 +1,26 @@
 package java_language.Classes;
 
 public class ReturnObjects {
+    int i;
+
+    ReturnObjects(int i) {
+        this.i = i;
+    }
+    // This method, takes an object, increases its 'i'
+    // and returns it.
+
+    ReturnObjects increaseByTen(ReturnObjects s) {
+        return new ReturnObjects(s.i+=10);
+    }
+
+}
+
+class TestReturnObjects {
+    public static void main(String[] args) {
+        ReturnObjects r = new ReturnObjects(10);
+        ReturnObjects o;
+        o = r.increaseByTen(r);
+        System.out.println("The value of 'o' is ---->>> " + o.i);
+        System.out.println("The value of r.i is --->>> " + r.i);
+    }
 }
