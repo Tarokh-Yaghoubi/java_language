69d4e3caa6880d5aa97dc7565b84681b617bd01d
yaghoubi <tarokh.yaghoubi@ENIAC-TECH.COM>
2025-04-05 16:34:06 +0330

    try-with-resources in java

--- exceptions/ExceptionsDemo.java
+++ exceptions/ExceptionsDemo.java
@@ -4,31 +4,37 @@ import org.jetbrains.annotations.NotNull;
 
 
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.FilterWriter;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class ExceptionsDemo {
     public static void show() {
-        FileReader reader = null;
-        try {
-            reader = new FileReader("file.txt");
-            reader.read();
-            new SimpleDateFormat().parse("");
 
+        // This is what we call try-with-resources
+        // there is no need for finally block in this way
+
+        try (
+                var reader = new FileReader("file.txt");
+                var writer = new FileWriter("...");
+        ) {
+            new SimpleDateFormat().parse("");
             System.out.println("File Opened");
         } catch (IOException | ParseException e) {
             throw new RuntimeException(e);
         }   // we can use the pipe | to treat all exceptions the same way
-        finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) { // test
-                    System.out.println(e.getMessage());
-                }
-            }
-        }
+//        finally {
+//            if (reader != null) {
+//                try {
+//                    reader.close();
+//                } catch (IOException e) { // test
+//                    System.out.println(e.getMessage());
+//                }
+//            }
+//        }
+
     }
 
     public static void sayHello(@NotNull String name) {
