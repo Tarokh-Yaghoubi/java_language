1b562954725e8155e9867a04275e67ba5dec3861
Tarokh Yaghoubi <tarokhgit@gmail.com>
2025-04-12 10:46:47 +0330

    Comparable<T o>> interface in java

---
+++ generics/User.java
@@ -0,0 +1,20 @@
+package generics;
+
+import org.jetbrains.annotations.NotNull;
+
+// The comparable Interface is for comparing two objects.
+// We need to override the compareTo method
+
+public class User implements Comparable<User>{
+
+    private int points;
+
+    public User (int points) {
+        this.points = points;
+    }
+
+    @Override
+    public int compareTo(@NotNull User user) {
+        return points - user.points;
+    }
+}
