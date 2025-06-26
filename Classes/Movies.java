e398abf5c66700c23e7a23b16f5edcb0d69d79d8
Tarokh Yaghoubi <tarokhgit@gmail.com>
2025-06-26 22:37:10 +0330

    Streams Demo - java programming

---
+++ Classes/Movies.java
@@ -0,0 +1,11 @@
+package Classes;
+
+public class Movies {
+    private String title;
+    private int likes;
+
+    public Movies(String title, int likes) {
+        this.title = title;
+        this.likes = likes;
+    }
+}
---
+++ hackerrank/LexigraphicalOrderedSubstrs.java
@@ -0,0 +1,9 @@
+package hackerrank;
+
+public class LexigraphicalOrderedSubstrs {
+    public static void main(String[] args) {
+        String str = "tarokhishere";
+        String[] strs = {""};
+
+    }
+}
